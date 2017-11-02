74e0d483d8d9785046ee2fa752a17426524a2bb7
Qin Yin <q.yin@live.com>
2017-11-02 14:55:05 -0700

    Find all references to Cells

--- Rubberduck.Inspections/Concrete/ParameterlessCellsInspection.cs
+++ Rubberduck.Inspections/Concrete/ParameterlessCellsInspection.cs
@@ -1,30 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
+using Antlr4.Runtime;
 using Rubberduck.Common;
 using Rubberduck.Inspections.Abstract;
 using Rubberduck.Inspections.Results;
+using Rubberduck.Parsing;
 using Rubberduck.Parsing.Grammar;
 using Rubberduck.Parsing.Inspections.Abstract;
 using Rubberduck.Parsing.Inspections.Resources;
-using Rubberduck.Parsing.Symbols;
 using Rubberduck.Parsing.VBA;
-using Rubberduck.UI;
+using Rubberduck.VBEditor;
 
 namespace Rubberduck.Inspections.Concrete
 {
-    public sealed class ParameterlessCellsInspection : InspectionBase
+    public sealed class ParameterlessCellsInspection : ParseTreeInspectionBase
     {
         public ParameterlessCellsInspection(RubberduckParserState state)
             : base(state, CodeInspectionSeverity.Hint) { }
 
         public override CodeInspectionType InspectionType { get; } = CodeInspectionType.MaintainabilityAndReadabilityIssues;
 
+        public override IInspectionListener Listener { get; } = new ParameterlessCellsInspectionLister();
+
         protected override IEnumerable<IInspectionResult> DoGetInspectionResults()
         {
-            var testVar = State.DeclarationFinder.MatchName("Cells");
+            var cellsReferences = State.DeclarationFinder.MatchName("Cells")
+                .Where(member => member.AsTypeName == "Range"
+                                 && member.References.Any()
+                                 && member.ParentDeclaration.AsTypeName == "Range")
+                .SelectMany(declaration => declaration.References);
+
+            var context1 = Listener.Contexts.Where(context => !IsIgnoringInspectionResultFor(context.ModuleName, context.Context.Start.Line));
+
             return null;
+        }
+    }
+
+    public class ParameterlessCellsInspectionLister : VBAParserBaseListener, IInspectionListener
+    {
+        private readonly List<QualifiedContext<ParserRuleContext>> _contexts = new List<QualifiedContext<ParserRuleContext>>();
+
+        public IReadOnlyList<QualifiedContext<ParserRuleContext>> Contexts => _contexts;
+
+        public QualifiedModuleName CurrentModuleName { get; set; }
+
+        public void ClearContexts()
+        {
+            _contexts.Clear();
+        }
+
+        public override void ExitArgList(VBAParser.ArgListContext context)
+        {
+
         }
     }
 }
